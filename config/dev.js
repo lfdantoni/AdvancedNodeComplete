76b29cdb7db438d5ff78a2ca442c614f4aa0f451
Leonardo D'Antoni <ldantoni@solstice.com>
2019-07-24 19:56:56 -0300

    update

--- config/dev.js
+++ config/dev.js
@@ -5,8 +5,5 @@ module.exports = {
     '908539389090-gjosll2ej74bqc158sncrbh4l1o1gegu.apps.googleusercontent.com',
   googleClientSecret: 'KnH-dKatj6YyX1GvlmEBPMv1Drp3',
   mongoURI: 'mongodb://test:test123@ds235840.mlab.com:35840/heroku_g75m4l1q',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
-  secretAccessKey: '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'
+  cookieKey: '123123123'
 };
