5a60bd540000bc6e039f0258ed679c63c54e9f05
lfdantoni <lfdantoni@gmail.com>
2019-07-25 10:02:59 -0300

    fix

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@
 module.exports = {
   googleClientID:
     '908539389090-gjosll2ej74bqc158sncrbh4l1o1gegu.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-dKatj6YyX1GvlmEBPMv1Drp3',
+  googleClientSecret: 'dKatj6YyX1GvlmEBPMv1Drp3',
   mongoURI: 'mongodb://test:test123@ds235840.mlab.com:35840/heroku_g75m4l1q',
   cookieKey: '123123123',
   redisUrl: 'redis://redistogo:62b58aea225a1c8541052443a40ebb14@spinyfin.redistogo.com:10627',
